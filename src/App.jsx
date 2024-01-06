485866fb2178385b4a6a0802292c371f3590d2fa
Divnoor <divnoorsingh4602@gmail.com>
2024-01-07 00:43:00 +0530

    investment calculator inital commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// todo: Add a header component
+// todo:  user input component
+// todo: Handling events and two way binding
+// todo: Lifting state up
+// todo: Outputting reuslts
+
+function App() {
+  return <h1>React Investment Calculator</h1>;
+}
+
+export default App;
